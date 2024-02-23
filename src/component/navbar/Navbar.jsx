0d2552d1308677854f159aaf81ed0297026b3824
Daniel <danielkore1234@gmil.com>
2024-02-23 17:04:40 +0100

    added contact-page

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 
 import { Link } from 'react-router-dom'
-
+import call from '../../assets/call-nav.svg'
+import clock from '../../assets/clock-nav.svg'
+import location from '../../assets/maps-and-flags -nav.svg'
 
 const Navbar = () => {
   return (
     <div className='text-white'>
        <div className='max-w-screen-2xl mx-auto w-11/12'>
        <div className='flex justify-between items-center '>
-        <div className='flex' >
-            <p>(217)2433434</p>
+        <div className='flex items-center justify-center gap-1' >
+            <img className='w-3 h-3 gap-4 text-white' src={call} alt="" />
+            <p className='pr-2'>(217)2433434</p>
+            <img className='w-3 h-3' src={clock} alt="" />
             <p>Monday - Friday: 8:00 AM - 4:30 PM</p>
         </div>
         <div>
@@ -16,7 +20,8 @@ const Navbar = () => {
           <img className='w-80' src="https://michaelhankinslaw.com/wp-content/uploads/2023/05/michael-hankins-law-office-jacksonville-il-logo-new.png" alt="" />
           </Link>
         </div>
-        <div>
+        <div className='flex gap-1 justify-center items-center'>
+            <img className='w-3 h-3' src={location} alt="" />
             <p>301 W. State Street Jacksonville, Illinois 62650</p>
         </div>
         </div>
--- src/pages/contact/contactContent/section2/Section2.jsx
+++ src/pages/contact/contactContent/section2/Section2.jsx
@@ -1,4 +1,7 @@
 import mapImg from '../../../../assets/newmap.jpg'
+import call from "../../../../assets/call.svg"
+import clock from "../../../../assets/clock.svg"
+import location from "../../../../assets/maps-and-flags.svg"
 
 const Section2 = () => {
   return (
@@ -9,20 +12,23 @@ const Section2 = () => {
                     <h1 className='text-2xl text-[#16163f] py-6'>Contact Our Law Firm And Speak With An Attorney In Jacksonville Illinois</h1>
                     <p className='text-xl text-[#211f40] pb-10'>Regardless if your legal problems seem minor or you anticipate a difficult legal battle, you will need the help of a lawyer to guide you through the process of the legal system. Call the office of Michael A. Hankins for all your legal advice</p>
                     <div className='flex flex-col  gap-7 justify-center items-center text-center'>
-                        <div>
+                        <div className='flex flex-col justify-center items-center' >
+                            <img className='w-12 h-12 pb-4' src={call} alt="" />
                         <hr className='border-t-2 border-yellow-300 pt-3 w-1/2 mx-auto' />
                             <h2 className='text-3xl'>Phone</h2>
                             <p className='text-[18px]'>(217) 243-3434</p>
                         </div>
                         <div className='flex flex-row gap-16'>
-                            <div>
+                            <div className='flex flex-col justify-center items-center'>
                                 <hr className='border-t-2 border-yellow-300 pt-3 w-1/2 mx-auto' />
+                                <img className='w-12 h-12 pb-4' src={location} alt="" />
                                 <h2 className='text-3xl '>Office Location</h2>
                                 <p className='text-[18px] '>301 W. State Street</p>
                                 <p className='text-[18px] '> Jacksonville, Illinois 62650</p>
                             </div>
-                            <div>
+                            <div className='flex flex-col justify-center items-center'>
                             <hr className='border-t-2 border-yellow-300 pt-3 w-1/2 mx-auto' />
+                            <img className='w-12 h-12 pb-4' src={clock} alt="" />
                                 <h2 className='text-3xl '>Office Hours</h2>
                                 <p className='text-[18px] '>Monday - Friday</p>
                                 <p className='text-[18px] '>Monday - Friday</p>
